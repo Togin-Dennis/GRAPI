9c37403ad6904d68ddb771c463b8f7494c9c91eb
Togin Dennis <tcodeappscust@gmail.com>
2025-06-16 16:50:03 +0530

    Updates

---
+++ App.js
@@ -0,0 +1,52 @@
+// App.js
+import React, { useContext } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
+import Header from './Home/Header/Header';
+import Search from './Search/Search';
+import LoadingScreen from './Loading/Loading';
+import './App.css';
+import { GameDealsContext } from './GlobelFunc/Globeldatastore';
+import NotFound from './404/404';
+
+function App() {
+  const {
+    Steamtopdeals,
+    Epictopdeals,
+    gogtopdeals,
+    Ubisofttopdeals
+  } = useContext(GameDealsContext);
+
+
+
+
+
+  const allDataLoaded =
+    Steamtopdeals.length > 0 &&
+    Epictopdeals.length > 0 &&
+    gogtopdeals.length > 0 &&
+    Ubisofttopdeals.length > 0;
+
+
+  return (
+    <Router>
+      {!allDataLoaded ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <div className='HeaderFixed'>
+            <Header />
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/loading" element={<LoadingScreen />} />
+             <Route path="/404" element={<NotFound/>} />
+          </Routes>
+        </>
+      )}
+    </Router>
+  );
+}
+
+export default App;
